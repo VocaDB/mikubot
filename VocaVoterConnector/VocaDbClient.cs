38ac30edd200bc17707c378ce43f895478756fee
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-02-14 19:11:35 +1000

    Implement VocaDbClient.GetSongById

--- VocaVoterConnector/VocaDbClient.cs
+++ VocaVoterConnector/VocaDbClient.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using com.cardinal.voca4j;
+using com.cardinal.voca4j.api;
+using com.cardinal.voca4j.api.entities.song;
+using com.cardinal.voca4j.api.song;
+using com.cardinal.voca4j.api.song.get;
+using com.cardinal.voca4j.impl;
 using VocaDb.Model.DataContracts.Albums;
 using VocaDb.Model.DataContracts.Api;
 using VocaDb.Model.DataContracts.Artists;
@@ -7,14 +13,27 @@ using VocaDb.Model.DataContracts.Songs;
 using VocaDb.Model.DataContracts.Tags;
 using VocaDb.Model.DataContracts.Users;
 using VocaDb.Model.Domain.Globalization;
-using VocaDb.Model.Domain.PVs;
 using VocaDb.Model.Service;
+using VocaDbNameMatchMode = VocaDb.Model.Service.NameMatchMode;
+using VocaDbPVService = VocaDb.Model.Domain.PVs.PVService;
+using VocaDbSongType = VocaDb.Model.Domain.Songs.SongType;
 
 namespace MikuBot.VocaDBConnector
 {
+	internal static class APIServiceExtensions
+	{
+		public static T completeAndParse<T>(this APIService apiService, Request request) => (T)apiService.completeAndParseGenerically(request);
+	}
+
+	internal static class BuilderProviderExtensions
+	{
+		public static T getBuilder<T>(this BuilderProvider builderProvider) where T : StateControlledBuilder => (T)builderProvider.getBuilder(typeof(T));
+	}
+
 	public sealed class VocaDbClient : IDisposable
 	{
 		private readonly string _endpoint;
+		private readonly Voca4J _voca4j = new();
 
 		public VocaDbClient(string endpoint)
 		{
@@ -34,7 +53,7 @@ namespace MikuBot.VocaDBConnector
 		~VocaDbClient() => Dispose(false);
 
 		public PartialFindResult<AlbumContract> FindAlbums(string term, int maxResults,
-			NameMatchMode nameMatchMode = NameMatchMode.Auto, AlbumSortRule sort = AlbumSortRule.NameThenReleaseDate)
+			VocaDbNameMatchMode nameMatchMode = VocaDbNameMatchMode.Auto, AlbumSortRule sort = AlbumSortRule.NameThenReleaseDate)
 		{
 			throw new NotImplementedException();
 		}
@@ -49,12 +68,12 @@ namespace MikuBot.VocaDBConnector
 			throw new NotImplementedException();
 		}
 
-		public PartialFindResult<ArtistContract> FindArtists(string term, int maxResults, NameMatchMode nameMatchMode = NameMatchMode.Auto)
+		public PartialFindResult<ArtistContract> FindArtists(string term, int maxResults, VocaDbNameMatchMode nameMatchMode = VocaDbNameMatchMode.Auto)
 		{
 			throw new NotImplementedException();
 		}
 
-		public PartialFindResult<SongWithAlbumAndPVsContract> FindSongs(string term, int maxResults, NameMatchMode nameMatchMode = NameMatchMode.Auto)
+		public PartialFindResult<SongWithAlbumAndPVsContract> FindSongs(string term, int maxResults, VocaDbNameMatchMode nameMatchMode = VocaDbNameMatchMode.Auto)
 		{
 			throw new NotImplementedException();
 		}
@@ -86,10 +105,32 @@ namespace MikuBot.VocaDBConnector
 
 		public SongDetailsContract GetSongById(int id, ContentLanguagePreference? language)
 		{
-			throw new NotImplementedException();
+			var builder = _voca4j.getBuilderProvider().getBuilder<SongGet.SongGetBuilder>();
+			var request = builder
+				.setID(id)
+				.setLanguage(Language.valueOf(language.ToString()))
+				.setFields(new[] { SongField.AdditionalNames })
+				.build();
+			Constants.rootURL = _endpoint;
+			var song = _voca4j.getApiService().completeAndParse<Song>(request);
+
+			return new SongDetailsContract
+			{
+				Song = new SongContract
+				{
+					AdditionalNames = song.getAdditionalNames(),
+					ArtistString = song.getArtistString(),
+					Id = song.getId(),
+					LengthSeconds = song.getLengthSeconds(),
+					Name = song.getName(),
+					PublishDate = DateTime.TryParse(song.getPublishDate().toString().ToString(), out var publishDate) ? publishDate : null,
+					RatingScore = song.getRatingScore(),
+					SongType = Enum.TryParse<VocaDbSongType>(song.getSongType().name(), out var songType) ? songType : VocaDbSongType.Unspecified,
+				},
+			};
 		}
 
-		public SongDetailsContract GetSongDetails(string term, ContentLanguagePreference? language = null, NameMatchMode matchMode = NameMatchMode.Auto)
+		public SongDetailsContract GetSongDetails(string term, ContentLanguagePreference? language = null, VocaDbNameMatchMode matchMode = VocaDbNameMatchMode.Auto)
 		{
 			throw new NotImplementedException();
 		}
@@ -104,7 +145,7 @@ namespace MikuBot.VocaDBConnector
 			throw new NotImplementedException();
 		}
 
-		public Task<SongWithAlbumContract> GetSongWithPVAsync(PVService service, string pvId)
+		public Task<SongWithAlbumContract> GetSongWithPVAsync(VocaDbPVService service, string pvId)
 		{
 			throw new NotImplementedException();
 		}
